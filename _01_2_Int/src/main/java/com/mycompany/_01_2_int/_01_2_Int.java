27701e6906d66ac9c590329a404ee68690291162
LENOVO <hfz.snyl@gmail.com>
2025-02-17 22:08:53 +0300

    Java-Examples

---
+++ _01_2_Int/src/main/java/com/mycompany/_01_2_int/_01_2_Int.java
@@ -0,0 +1,12 @@
+package com.mycompany._01_2_int;
+
+public class _01_2_Int {
+
+    public static void main(String[] args) {
+        // YÖNTEM
+        int s1 = 5;
+        // YÖNTEM 2
+        int s2 = 5, s3 = 10;
+        double s4 = 1.4;
+    }
+}
