1e51f94bb974f6cd193c4591cf4f37753ac3dd59
LENOVO <hfz.snyl@gmail.com>
2025-02-17 22:12:42 +0300

    Java-Examples

---
+++ _01_3_Type/src/main/java/com/mycompany/_01_3_type/_01_3_Type.java
@@ -0,0 +1,26 @@
+
+package com.mycompany._01_3_type;
+
+public class _01_3_Type {
+
+    public static void main(String[] args) {
+ // Yöntem 1
+
+        int s1 = 5;
+// Yöntem 2
+        int s2 = 5, s3 = 10;
+        double s4 = 20.4;
+// Yöntem 3
+        int s5;
+        s5 = 10;
+// Yöntem 4
+        int s6 = 5, s7;
+        s7 = 10;
+// Yöntem 5
+        int s8, s9;
+        s8 = 10;
+        s9 = 20;
+// int vs. olmadığı sürece yanyana atama yapılamaz.
+// örnek olmaz: s8 = 10, s9 = 20; (hata verir)
+    }
+}
