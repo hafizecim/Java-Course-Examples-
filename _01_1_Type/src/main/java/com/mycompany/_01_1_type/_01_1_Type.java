dc46eb6c95decdd19d92b5b7cb4efb18b89568d0
LENOVO <hfz.snyl@gmail.com>
2025-02-17 21:57:15 +0300

    Java-Examples

---
+++ _01_1_Type/src/main/java/com/mycompany/_01_1_type/_01_1_Type.java
@@ -0,0 +1,22 @@
+
+package com.mycompany._01_1_type;
+
+public class _01_1_Type {
+
+    public static void main(String[] args) {
+// TAMSAYI DEĞİŞKEN TİPLERİ
+        int sayi = 5;
+// ONDALIKLI SAYI DEĞİŞKEN TİPLERİ
+        float ondalikliSayi1 = 1.5F;
+        /* float olduğu için belitmek
+için sayının sonuna F yada f yazılmalı*/
+        double ondalikliSayi2 = 10.54;
+// KARAKTER DEĞİŞKEN TİPLERİ
+        char karakter = 'a';
+// METİN DEĞİŞKEN TİPLERİ
+        String metin = "Hafize";
+// MANTIKSAL DEĞİŞKEN TİPİ
+        boolean durum = true;
+        /* sadece true yada false alabilir*/
+    }
+}
